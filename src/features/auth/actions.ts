ddfbb1a994c7c679796005cca6ee3df1c6e2feed
Samarth Verma <vsamarthj@gmail.com>
2025-01-16 22:47:48 +0800

    Remove signup page

--- src/features/auth/actions.ts
+++ src/features/auth/actions.ts
@@ -1,18 +1,17 @@
 "use server";
 
-import { signIn } from "@/lib/auth";
+import { signIn as authSignIn } from "@/lib/auth";
 import { CredentialsSignin } from "next-auth";
-export default async function action(_: unknown, formData: FormData) {
+export async function signIn(_: unknown, formData: FormData) {
   try {
     const data = Object.fromEntries(formData);
     const { email, password } = data;
-    await signIn("credentials", {
+    await authSignIn("credentials", {
       email,
       password,
       redirectTo: "/",
     });
   } catch (error) {
-    console.error("sign in", error);
     if (error instanceof CredentialsSignin) {
       return "Incorrect email or password";
     }
